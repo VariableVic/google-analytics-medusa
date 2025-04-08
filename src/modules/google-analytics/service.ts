35e0869699df75244b0698d844ebd6f4494a63bd
Victor Gerbrands <victorgerbrands@gmail.com>
2025-04-08 12:21:09 +0200

    fix: service naming conventions, logging

--- src/modules/google-analytics/service.ts
+++ src/modules/google-analytics/service.ts
@@ -1,3 +1,4 @@
+import { logger } from "@medusajs/framework";
 import { GA4Event, GA4EventPayload } from "../../types";
 import { PREDEFINED_EVENTS } from "../../utils/predefined-event-params";
 
@@ -5,13 +6,15 @@ type ModuleOptions = {
   apiSecret: string;
   measurementId: string;
   debug?: boolean;
+  maxEvents?: number;
 };
 
 class GoogleAnalyticsService {
   protected options: ModuleOptions;
-  private readonly GA_ENDPOINT: string;
-  private readonly DEBUG_ENDPOINT: string;
-  private readonly DEBUG: boolean;
+  private readonly gaEndpoint: string;
+  private readonly debugEndpoint: string;
+  private readonly debug: boolean;
+  private readonly maxEvents: number;
   static readonly PREDEFINED_EVENTS = PREDEFINED_EVENTS;
 
   constructor({}, options: ModuleOptions) {
@@ -22,29 +25,30 @@ class GoogleAnalyticsService {
     }
 
     this.options = options;
-    this.DEBUG = options.debug || false;
-    this.GA_ENDPOINT = `https://www.google-analytics.com/mp/collect?measurement_id=${this.options.measurementId}&api_secret=${this.options.apiSecret}`;
-    this.DEBUG_ENDPOINT = `https://www.google-analytics.com/debug/mp/collect?measurement_id=${this.options.measurementId}&api_secret=${this.options.apiSecret}`;
+    this.debug = options.debug || false;
+    this.maxEvents = options.maxEvents || 25;
+    this.gaEndpoint = `https://www.google-analytics.com/mp/collect?measurement_id=${this.options.measurementId}&api_secret=${this.options.apiSecret}`;
+    this.debugEndpoint = `https://www.google-analytics.com/debug/mp/collect?measurement_id=${this.options.measurementId}&api_secret=${this.options.apiSecret}`;
   }
 
   async send(payload: GA4EventPayload) {
-    const MAX_EVENTS = 25;
-
     this.checkPredefinedEventParams(payload.events);
-    this.nullParamCheck(payload.events);
+    this.checkNullParams(payload.events);
 
-    if (payload.events.length > MAX_EVENTS) {
+    if (payload.events.length > this.maxEvents) {
       await this.sendLargeEventCount(payload);
     } else {
-      await this._sendEvents(payload);
+      await this.sendEvents(payload);
     }
   }
 
-  private nullParamCheck(events: GA4Event[]) {
+  private checkNullParams(events: GA4Event[]) {
     events.forEach((event) => {
       Object.keys(event.params).forEach((param) => {
         if (event.params[param] === null) {
-          console.log(`Removed null param for '${param}' in '${event.name}'`);
+          logger.info(
+            `GA4 Plugin: Removed null param for '${param}' in '${event.name}'`
+          );
           delete event.params[param];
         }
       });
@@ -52,15 +56,14 @@ class GoogleAnalyticsService {
   }
 
   private async sendLargeEventCount(payload: GA4EventPayload) {
-    const MAX_EVENTS = 25;
     while (payload.events.length > 0) {
-      const batch = payload.events.splice(0, MAX_EVENTS);
-      await this._sendEvents({ ...payload, events: batch });
+      const batch = payload.events.splice(0, this.maxEvents);
+      await this.sendEvents({ ...payload, events: batch });
     }
   }
 
-  async _sendEvents(payload: GA4EventPayload) {
-    const url = this.DEBUG ? this.DEBUG_ENDPOINT : this.GA_ENDPOINT;
+  private async sendEvents(payload: GA4EventPayload) {
+    const url = this.debug ? this.debugEndpoint : this.gaEndpoint;
 
     try {
       const response = await fetch(url, {
@@ -77,9 +80,9 @@ class GoogleAnalyticsService {
         );
       }
 
-      console.log("GA4 Plugin: Events sent to Google Analytics successfully");
+      logger.info("GA4 Plugin: Events sent to Google Analytics successfully");
     } catch (error) {
-      console.error(
+      logger.error(
         "GA4 Plugin: Error sending events to Google Analytics:",
         error
       );
@@ -92,8 +95,8 @@ class GoogleAnalyticsService {
         GoogleAnalyticsService.PREDEFINED_EVENTS[event.name];
       if (requiredParams) {
         requiredParams.forEach((param) => {
-          if (this.DEBUG && !(param in event.params)) {
-            console.log(
+          if (this.debug && !(param in event.params)) {
+            logger.info(
               `GA4 Plugin: Missing a recommended parameter "${param}" for "${event.name}"`
             );
           }
